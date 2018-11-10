53244aa9b96577230345d470cad9d1a57eed03b3
shy147 <shy147@shy147-31646.pods.shy147.svc.cluster.local>
2018-11-09 22:03:38 -0800

    Saved filters as npy files

--- visualize.py
+++ visualize.py
@@ -11,12 +11,16 @@ import os
 
 def main():
     model_weights = torch.load("./best_baseline_model.pt")
-    print(model_weights['conv1.weight'].size())
-    print(model_weights['conv2.weight'].size())
-    print(model_weights['conv3.weight'].size())
+    np.save("baseline_conv1.npy", model_weights['conv1.weight'].cpu().numpy())
+    np.save("baseline_conv2.npy", model_weights['conv2.weight'].cpu().numpy())
+    np.save("baseline_conv3.npy", model_weights['conv3.weight'].cpu().numpy())
+
+    conv1_weights = np.load("baseline_conv1.npy")
+    conv2_weights = np.load("baseline_conv2.npy")
+    conv3_weights = np.load("baseline_conv3.npy")
     fit =plt.figure()
     plt.figure(figsize=(10,10))
-    for idx, filt in enumerate(model_weights['conv1.weight']):
+    for idx, filt in conv1_weights:
         plt.subplot(4,8,idx + 1)
         plt.imshow(filt[0,:,:], cmap="gray")
         plt.axis('off')
